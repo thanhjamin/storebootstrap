ba9bbb32383d928f82bf2e1929d078ec932862d0
Thanh Huynh <thanhjamin@yahoo.com>
2013-10-10 14:55:13 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Store - Bootstrap
+By Thanh Huynh
+
+Description
+A very basic app using bootstrap for styling. Introductory lesson about bootstrap on Rails Cast #328.
+
+Installation
+Visit http://railscasts.com/episodes/328-twitter-bootstrap-basics for more information
+
+Usage
+An example of the utilization of bootstrap
+
+Screenshot
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Storebootstrap::Application.config.secret_key_base = '2d9ad4c182fce70a7d673ffed19d0b06fe9551303d9e3f0518583d94eac8d27300de0b67c9308efae43581cefdc5dc13581c0b68ccc198e9ad92b299610b07cf'
